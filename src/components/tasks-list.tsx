15d9c40843d189dd4d059f7fe3aed1da52080d14
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-03-15 10:57:57 -0300

    feat: adicionando listagem de tarefas a home

--- src/components/tasks-list.tsx
+++ src/components/tasks-list.tsx
@@ -1,4 +1,27 @@
-type 
-export function TasksList({}:){
+import React from 'react';
+import { Text, View } from 'react-native';
 
+import { Task } from '~/@types';
+
+export function TasksList() {
+  const tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Tomar Cafe',
+      status: false,
+      description: 'comer uma proteina e uma fruta obrigatoriamente',
+    },
+  ];
+  return (
+    <View className="w-4/5">
+      {tasks.map((task) => (
+        <View className={styles.container} key={task.id}>
+          <Text>{task.title}</Text>
+        </View>
+      ))}
+    </View>
+  );
 }
+const styles = {
+  container: `flex flex-row w-full mx-3 border border-solid border-current`,
+};
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -3,12 +3,23 @@ import { Text, View } from 'react-native';
 
 import EditScreenInfo from '../components/edit-screen-info';
 
+import { TasksList } from '~/components/tasks-list';
+
 const Page = () => {
+  const day = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+  const date = `${day}/0${month}/${year}`;
+
   return (
     <View className={styles.container}>
+      <View className={styles.content}>
+
       <Text className={styles.title}>Suas Atividades</Text>
+      <Text className={styles.title}>{date}</Text>
+      </View>
       <View className={styles.separator} />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <TasksList />
     </View>
   );
 };
@@ -16,7 +27,8 @@ const Page = () => {
 export default Page;
 
 const styles = {
-  container: `items-center flex-1 justify-center`,
+  container: `items-center flex-1 justify-start mt-4`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
   title: `text-xl font-bold`,
+  content: `flex flex-row justify-around w-full`,
 };
