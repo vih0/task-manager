a9f581baecb72e3f6b57fd5d9a55fc731e057ea6
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-03-16 17:00:34 -0300

    refactor: melhorano data da tela inicial

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,25 +1,28 @@
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 import EditScreenInfo from '../components/edit-screen-info';
 
 import { TasksList } from '~/components/tasks-list';
 
 const Page = () => {
-  const day = new Date().getDate();
-  const month = new Date().getMonth();
-  const year = new Date().getFullYear();
-  const date = `${day}/0${month}/${year}`;
+  const day = new Date();
+  const formatedDate = Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'short',
+  }).format(day);
 
   return (
     <View className={styles.container}>
       <View className={styles.content}>
-
-      <Text className={styles.title}>Suas Atividades</Text>
-      <Text className={styles.title}>{date}</Text>
+        <Text className={styles.title}>Suas Atividades</Text>
+        <Text className={styles.title}>{formatedDate}</Text>
       </View>
       <View className={styles.separator} />
       <TasksList />
+      <Pressable className={styles.button}>
+        <Ionicons name="add-outline" size={30} color="white" />
+      </Pressable>
     </View>
   );
 };
@@ -27,8 +30,9 @@ const Page = () => {
 export default Page;
 
 const styles = {
-  container: `items-center flex-1 justify-start mt-4`,
+  container: `items-center flex-1 justify-start mt-4 relative`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
   title: `text-xl font-bold`,
   content: `flex flex-row justify-around w-full`,
+  button: `p-3 bg-blue-700 rounded-full absolute bottom-20 right-10 active:bg-blue-800`,
 };
