1f44a193ac1e5631b7c43a9dc7b7371403d3fe48
vitoria.araujo <vitoria.araujo@ceuma.com>
2024-03-18 17:16:06 -0300

    feat: adicionando tipos de tarefas e array tarefa

--- src/@types/Task.ts
+++ src/@types/Task.ts
@@ -1,7 +1,14 @@
-export type Task = {
+export type Tarefa = {
   id: number;
-  title: string;
-  description: string;
-  status: boolean;
-  priority: number;
+  titulo: string | null;
+  descricao: string | null;
+  concluido: boolean | null;
+  dataCriacao: string;
+  dataExclusao: string;
+  prioridade: {
+    id: number;
+    codigo: string;
+    cor: string;
+    descricao: string;
+  };
 };
---
+++ src/@types/Tasks.ts
@@ -0,0 +1,5 @@
+import { Tarefa } from './Task';
+
+export type Tarefas = {
+  tarefas: Tarefa[];
+};
